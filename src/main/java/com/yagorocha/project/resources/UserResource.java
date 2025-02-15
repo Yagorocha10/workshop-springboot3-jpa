923fbd45935fc6145b76186279676fae90969f77
yago rocha <yagodasilvarocha30@gmail.com>
2025-02-15 15:11:14 -0300

    User entity and resource

---
+++ src/main/java/com/yagorocha/project/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.yagorocha.project.resources;
+
+
+import com.yagorocha.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User>findALL() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+
+
+
+
+
+
+
+}
