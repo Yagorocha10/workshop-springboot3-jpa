84441b1c632c9c7fe5c90a772957c7220973c030
yago rocha <yagodasilvarocha30@gmail.com>
2025-02-21 16:34:02 -0300

    OrderStatus enum

--- src/main/java/com/yagorocha/project/config/TestConfig.java
+++ src/main/java/com/yagorocha/project/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.yagorocha.project.config;
 
 import com.yagorocha.project.entities.Order;
 import com.yagorocha.project.entities.User;
+import com.yagorocha.project.entities.enums.OrderStatus;
 import com.yagorocha.project.repositories.OrderRepository;
 import com.yagorocha.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig  implements CommandLineRunner {
         User u1 = new User(null, "Maria brown", "maria@gmail.com", "988", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977", "123456");
 
-        Order O1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order O2 = new Order(null, Instant.parse("2019-07-21t03:42:10Z"), u2);
-        Order O3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order O1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order O2 = new Order(null, Instant.parse("2019-07-21t03:42:10Z"), OrderStatus.WAITING_PAYMENT,  u2);
+        Order O3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,  u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(O1, O2,  O3));
