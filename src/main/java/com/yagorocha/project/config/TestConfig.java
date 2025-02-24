6d2d6babe96998c738f3d23eba3ba9ca07e92d5b
yago rocha <yagodasilvarocha30@gmail.com>
2025-02-24 16:23:20 -0300

    Payment, one-to-one association

--- src/main/java/com/yagorocha/project/config/TestConfig.java
+++ src/main/java/com/yagorocha/project/config/TestConfig.java
@@ -85,6 +85,11 @@ public class TestConfig  implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        Payment pay1 = new Payment(null,  Instant.parse("2019-06-20T21:53:07Z"), O1);
+        O1.setPayment(pay1);
+
+        orderRepository.save(O1);
+
 
 
 
