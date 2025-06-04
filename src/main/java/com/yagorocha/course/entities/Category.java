2f4b4b84e00a942e07741a99ad9f707f3d76dbc1
Yago rocha <yagodasilvarocha30@gmail.com>
2025-06-04 16:29:24 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/yagorocha/course/entities/Category.java
+++ src/main/java/com/yagorocha/course/entities/Category.java
@@ -8,16 +8,16 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-@Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
+    @Entity
+    @Table(name = "tb_category")
+    public class Category implements Serializable {
+        private static final long serialVersionUID = 1L;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
 
-    @JsonIgnore
+        @JsonIgnore
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
