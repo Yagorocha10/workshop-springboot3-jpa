a8a368db04b92faabdb5060661428ac5039ab12a
Yago rocha <yagodasilvarocha30@gmail.com>
2025-06-04 16:45:12 -0300

    Payment, one-to-one association

--- src/main/java/com/yagorocha/course/entities/Order.java
+++ src/main/java/com/yagorocha/course/entities/Order.java
@@ -25,9 +25,13 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
-
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
+
+    //mapeando para ter o mesmo código no caso payment e order
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+
+    private Payment payment;
 
     public Order() {
     }
@@ -71,6 +75,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems() {
