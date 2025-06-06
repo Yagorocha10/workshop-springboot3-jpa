c159167622ddbb087c7b4e6c8378160de2b84170
Yago rocha <yagodasilvarocha30@gmail.com>
2025-06-06 01:12:49 -0300

    Exception handling - delete

--- src/main/java/com/yagorocha/course/resources/exceptions/ResouceExceptionHandler.java
+++ src/main/java/com/yagorocha/course/resources/exceptions/ResouceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.yagorocha.course.resources.exceptions;
 
+import com.yagorocha.course.services.exceptions.DatabaseException;
 import com.yagorocha.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -19,4 +20,14 @@ public class ResouceExceptionHandler  {
             StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
             return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
 }
