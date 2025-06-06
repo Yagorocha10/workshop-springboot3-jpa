8bff9885cad295a530371b7b1db01df45c373682
Yago rocha <yagodasilvarocha30@gmail.com>
2025-06-06 12:42:15 -0300

    Exception handling - update

--- src/main/java/com/yagorocha/course/services/UserService.java
+++ src/main/java/com/yagorocha/course/services/UserService.java
@@ -4,11 +4,13 @@ import com.yagorocha.course.entities.User;
 import com.yagorocha.course.repositories.UserRepository;
 import com.yagorocha.course.services.exceptions.DatabaseException;
 import com.yagorocha.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.Optional;
 
@@ -43,13 +45,17 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
-
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+           throw new ResourceNotFoundException(id);
+        }
 
 
     }
+
 
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
